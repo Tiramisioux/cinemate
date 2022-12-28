91b21800fbc466b99ced02374f979b80d3dbdc3a
Patrik <patrik.m.eriksson@gmail.com>
2022-12-28 20:23:30 +0100

    message

--- rec_signal.py
+++ rec_signal.py
@@ -1,37 +1,27 @@
 import time
 import RPi.GPIO as GPIO
 from signal import pause
-from pysinewave import SineWave
 from time import sleep
 
 ledPin = 20
-pwmPin = 23
+pwmPin = 41
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
+GPIO.setwarnings(True)
 GPIO.setup(ledPin, GPIO.IN)
 GPIO.setup(pwmPin, GPIO.OUT)
 
-p = GPIO.PWM(pwmPin, 82.41) 
-
-recording = 0
+p = GPIO.PWM(pwmPin, 50)
 
 def rec_detect(channel):
     if GPIO.input(ledPin):
         print("recording")
-        p.start(50)
+        p.start(20)
 
     if not GPIO.input(ledPin):
         print("stopped recording")
         p.stop()
 
-print("start")
-
 GPIO.add_event_detect(ledPin, GPIO.BOTH, callback=rec_detect)
 
-
 pause()
-    
-
-

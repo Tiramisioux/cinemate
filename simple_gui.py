6a267e6cfd975ea41c25ea028bd2957a495e5df1
Tiramisiox <patrik.m.eriksson@gmail.com>
2023-03-02 23:28:37 +0100

    added simple-manual_controls.py example

--- simple_gui.py
+++ simple_gui.py
@@ -6,8 +6,7 @@ from shutil import disk_usage
 from gpiozero import CPUTemperature
 import psutil
 import redis
-from redis_proxy import CameraParameters
-import multiprocessing
+from redis_proxy import CameraParameters, RedisMonitor
 import time
 import RPi.GPIO as GPIO
 
@@ -26,23 +25,20 @@ cy = fb.size[1] // 2
 fill_color = "black"
 
 r = redis.Redis(host='localhost', port=6379, db=0)
+
+# Load monitoring
+monitor = RedisMonitor('cp_stats')
     
 # Remove cursor from shell
 os.system('sudo sh -c "TERM=linux setterm -foreground black -clear all >/dev/tty0"')
-
-# Connect to Redis
-r = redis.Redis(host='localhost', port=6379, db=0)
 
 # Make control objects
 iso_setting = CameraParameters("ISO")
 shutter_a_setting = CameraParameters("SHUTTER")
 fps_setting = CameraParameters("FPS")
 resolution_setting = CameraParameters("RESOLUTION")
-is_recording_setting = CameraParameters("IS_RECORDING")
-
 
 def check_drive_mounted():
-
     mount_point = "/media/RAW"
     if os.path.ismount(mount_point):
         # Get disk usage
@@ -51,40 +47,20 @@ def check_drive_mounted():
         if resolution_setting.get() == "1080": file_size = 3.2
         if resolution_setting.get() == "1520": file_size = 4.8
         min_left = int((free_bytes / 1000000) / (file_size * int(fps) * 60))
-        
     else:
         drive_mounted = 0
         min_left = None
-    
     return drive_mounted, min_left
 
-def check_last_clip_folder_name():
-    
-    # get a list of all directories in the folder_path
-    dirs = os.listdir('/media/RAW')
-
-    # sort the directories by their creation time (mtime)
-    dirs = sorted(dirs, key=lambda d: os.path.getmtime(os.path.join('/media/RAW', d)))
-
-    # get the last (most recently created) directory
-    if dirs: 
-        last_dir = dirs[-1]
-    else:
-        last_dir = ""
-    
-    return last_dir
-
-
 def get_values():
-    global iso, shutter_a, fps, resolution, is_recording
+    global iso, shutter_a, fps, resolution
     
     iso = iso_setting.get()
     shutter_a = shutter_a_setting.get()
     fps = fps_setting.get()
     resolution = resolution_setting.get()
-    is_recording = is_recording_setting.get()
     
-    return iso, shutter_a, fps, resolution, is_recording
+    return iso, shutter_a, fps, resolution
 
 def get_system_stats():
     # Get cpu statistics
@@ -97,11 +73,9 @@ def draw_display():
 
     fill_color = "black"
     
-    if is_recording == "1" and drive_mounted == 1:
+    if is_increasing == True:
         fill_color = "red"
-        GPIO.output(rec_out_pin,GPIO.HIGH)
-    if is_recording == "0":
-        GPIO.output(rec_out_pin,GPIO.LOW)
+    if is_increasing == False:
         fill_color = "black"
         
     image = Image.new("RGBA", fb.size)
@@ -126,22 +100,16 @@ def draw_display():
         draw.text((10, 1051), str((str(min_left)) + " min"), font = font, align ="left", fill="white")
     else:
         draw.text((10, 1051), "no disk", font = font, align ="left", fill="white")
-    
     #draw.text((160, 1051), str(resolution), font = font, align ="left", fill="white")
-    draw.text((722, 1051), str(last_clip), font = font, align ="left", fill="white")
             
     fb.show(image)
-    
-# Program
-
-GPIO.output(rec_out_pin,GPIO.LOW)
 
 # Subscribe to a pattern
 pubsub = r.pubsub()
 pubsub.psubscribe("cp_controls")
 
 # Get initial values
-iso, shutter_a, fps, resolution, is_recording = (get_values())
+iso, shutter_a, fps, resolution = get_values()
 resolution = int(resolution)
 
 file_size = 3.3
@@ -150,46 +118,23 @@ if resolution == 1080:
 if resolution == 1520: 
     file_size = 4.8
 drive_mounted, min_left = check_drive_mounted()
-if drive_mounted: 
-    last_clip = check_last_clip_folder_name()
-else:
-    last_clip = ""
-
 
 # Get cpu statistics
 cpu_load = str(psutil.cpu_percent()) + '%'
 cpu_temp = ('{}\u00B0'.format(int(CPUTemperature().temperature)))
-    
-# Draw display
-draw_display()
 
 while True:
-    iso, shutter_a, fps, resolution, is_recording = get_values()
+    
+    monitor.listen()
+
+    is_increasing = monitor.is_number_increasing()
+    
+    iso, shutter_a, fps, resolution = get_values()
     cpu_load, cpu_temp = get_system_stats()
     drive_mounted, min_left = check_drive_mounted()
-    if drive_mounted: 
-        last_clip = check_last_clip_folder_name()
-    else:
-        last_clip = ""
 
     draw_display()
-    time.sleep(0.5)
-
-# # Start listening for messages
-# for message in pubsub.listen():
-#     if message['type'] == 'pmessage':
-#         handle_message(message)
-    
-#         # Get camera parameters    
-#         awb, shu_a, iso, shutter_a, is_recording, shuter_a, shu_s, shutter_s, cam_init, compress, height, width, fps, io = (get_values())
-        
-#         # Draw display
-#         draw_display(int(is_recording))
-        
-#     elif message['type'] == 'punsubscribe':
-#         pubsub.close()
-#         break
-    
+    time.sleep(0.1)
     
 
 

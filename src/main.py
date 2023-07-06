23beb690f9268cf73f76cba32555a77972de6d9f
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2023-07-06 18:52:13 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ r = redis.Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=Config.REDIS_
 if __name__ == "__main__":
     try:
         # Instantiate and start the Monitor instance, for monitoring of SSD
-        monitor = DriveMonitor('/media/RAW/', rec_out_pins=[5], button_pins=[10])
+        monitor = DriveMonitor('/media/RAW/', rec_out_pins=[5], button_pins=[26])
         threading.Thread(target=monitor.start_space_monitoring, args=(0.1,)).start()
 
 
@@ -31,12 +31,12 @@ if __name__ == "__main__":
                                         iso_steps=[100, 200, 400, 800, 1600, 3200],    # Define an array for selectable ISO values (100-3200)
                                         shutter_angle_steps=list(range(1, 361)),       # Define an array for selectable shutter angle values (1-360 degrees)
                                         fps_steps=[1,2,4,8,16,18,24,25,33,48,50],      # Define an array for selectable fps values (1-50). To create and array of all frame rates from 1 - 50, replace the array with "list(range(1, 50))""
-                                        iso_pot=0,                                     # Analog channel for ISO control, if Grove Base HAT is attached (if defined, it overrides any iso_inc and iso_dec pins)
-                                        shutter_angle_pot=4,                           # Analog channel for shutter angle control, if Grove Base HAT is attached
-                                        fps_pot=2,                                     # Analog channel for fps control, if Grove Base HAT is attached
+                                        iso_pot=0,                                     # Analog channel for ISO control, if Grove Base HAT is attached (if defined, it overrides any iso_inc and iso_dec pins). Set to None if no Grove Base HAT is attached.
+                                        shutter_angle_pot=4,                           # Analog channel for shutter angle control, if Grove Base HAT is attached. Set to None if no Grove Base HAT is attached.
+                                        fps_pot=2,                                     # Analog channel for fps control, if Grove Base HAT is attached. Set to None if no Grove Base HAT is attached.
                                         iso_inc_pin=23,                                # GPIO pin for button for increasing ISO
                                         iso_dec_pin=25,                                # GPIO pin for button for decreasing ISO
-                                        pot_lock_pin=26,                               # GPIO pin for attaching shutter angle and fps potentiometer lock switch
+                                        pot_lock_pin=16,                               # GPIO pin for attaching shutter angle and fps potentiometer lock switch
                                         res_button_pin=24,                             # GPIO resolution button - switches between 1080 (cropped) and 1520 (full frame)
                                         fps_mult_pin1=18,                              # Flip switch for 50% frame rate
                                         fps_mult_pin2=19,                              # Flip switch for 200% frame rate (up to 50 fps)

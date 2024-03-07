84bc3e5fdf2b5040ef7c290875f97b53adb64c62
Patrik Eriksson <patrik.m.eriksson@gmail.com>
2024-03-07 02:41:42 +0100

    Updated gpio input mechanisms

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import threading
 import RPi.GPIO as GPIO
 from signal import pause
 import json
+import argparse
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -16,11 +17,9 @@ from module.ssd_monitor import SSDMonitor
 from module.gpio_output import GPIOOutput
 from module.cinepi_controller import CinePiController
 from module.simple_gui import SimpleGUI
-from module.gpio_input import GPIOInput
 from module.analog_controls import AnalogControls
 from module.grove_base_hat_adc import ADC
 from module.keyboard import Keyboard
-from module.system_button import SystemButton
 from module.cli_commands import CommandExecutor
 from module.serial_handler import SerialHandler
 from module.logger import configure_logging
@@ -30,6 +29,7 @@ from module.sensor_detect import SensorDetect
 from module.mediator import Mediator
 from module.dmesg_monitor import DmesgMonitor
 from module.redis_listener import RedisListener
+from module.gpio_input import ComponentInitializer, SmartButton, SimpleSwitch, RotaryEncoderWithButton 
 
 MODULES_OUTPUT_TO_SERIAL = ['cinepi_controller']
 
@@ -40,11 +40,23 @@ def load_settings(filename):
         shutter_a_steps = sorted(set(range(1, 361)).union(additional_shutter_a_steps))
         settings['shutter_a_steps'] = shutter_a_steps
         return settings
-
-if __name__ == "__main__":
-
-    logger, log_queue = configure_logging(MODULES_OUTPUT_TO_SERIAL)
     
+if __name__ == "__main__":
+    
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Run the CinePi application.")
+
+    # Add the debug argument
+    parser.add_argument("-debug", action="store_true", help="Enable debug logging level.")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Determine the logging level based on the presence of the -debug flag
+    logging_level = logging.DEBUG if args.debug else logging.INFO
+
+    logger, log_queue = configure_logging(MODULES_OUTPUT_TO_SERIAL, logging_level)
+
     settings = load_settings('/home/pi/cinemate/src/settings.json')
 
     # Detect sensor
@@ -60,6 +72,7 @@ if __name__ == "__main__":
     redis_controller = RedisController()
     ssd_monitor = SSDMonitor()
     usb_monitor = USBMonitor(ssd_monitor)
+    
     gpio_output = GPIOOutput(rec_out_pin=settings['rec_out_pin'])
     
     dmesg_monitor = DmesgMonitor("/var/log/kern.log")
@@ -75,24 +88,11 @@ if __name__ == "__main__":
                                         shutter_a_steps=settings['shutter_a_steps'],
                                         fps_steps=settings['fps_steps']
                                         )
+    
+    gpio_input = ComponentInitializer(cinepi_controller, settings)
 
     # Instantiate the AnalogControls component
     analog_controls = AnalogControls(cinepi_controller, iso_pot=settings['analog_controls']['iso_pot'], shutter_a_pot=settings['analog_controls']['shutter_a_pot'], fps_pot=settings['analog_controls']['fps_pot'])
-
-    # Instantiate the GPIOControls component
-    gpio_input = GPIOInput(cinepi_controller, redis_controller, **settings['gpio_input'])
-
-    # Instantiate SystemButton
-    system_button = SystemButton(cinepi_controller, redis_controller, ssd_monitor, **settings['system_button'])
-                                
-    # Instantiate a rotary encoder for ISO control
-    iso_encoder = SimpleRotaryEncoder(cinepi_controller, setting="iso", clk=settings['iso_encoder']['clk'], dt=settings['iso_encoder']['dt'])
-
-    # Instantiate a rotary encoder for shutter angle control
-    shu_encoder = SimpleRotaryEncoder(cinepi_controller, setting="shutter_a_nom", clk=settings['shu_encoder']['clk'], dt=settings['shu_encoder']['dt'])
-
-    # Instantiate a rotary encoder for fps control
-    fps_encoder = SimpleRotaryEncoder(cinepi_controller, setting="fps", clk=settings['fps_encoder']['clk'], dt=settings['fps_encoder']['dt'])
 
     # Instantiate the Mediator and pass the components to it
     mediator = Mediator(cinepi_app, redis_controller, usb_monitor, ssd_monitor, gpio_output)
@@ -104,15 +104,13 @@ if __name__ == "__main__":
     keyboard = Keyboard(cinepi_controller, usb_monitor)
     
     # Instantiate the CommandExecutor with all necessary components and settings
-    command_executor = CommandExecutor(cinepi_controller, system_button)
+    command_executor = CommandExecutor(cinepi_controller)
 
     # Start the CommandExecutor thread
     command_executor.start()
     
     serial_handler = SerialHandler(command_executor.handle_received_data, 9600, log_queue=log_queue)
     serial_handler.start()
-    
-
     
     redis_listener = RedisListener(redis_controller)
     
@@ -154,171 +152,3 @@ if __name__ == "__main__":
         
         # Cleanup GPIO pins
         GPIO.cleanup()
-
-
-# import logging
-# import sys
-# import traceback
-# import threading
-# import RPi.GPIO as GPIO 
-
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)  # Set pin numbering mode first
-
-# from signal import pause
-
-# from module.redis_controller import RedisController
-# from module.cinepi_app import CinePi
-# from module.usb_monitor import USBMonitor
-# from module.ssd_monitor import SSDMonitor
-# from module.gpio_output import GPIOOutput
-# from module.cinepi_controller import CinePiController
-# from module.simple_gui import SimpleGUI
-# from module.gpio_input import GPIOInput
-# from module.analog_controls import AnalogControls
-# from module.grove_base_hat_adc import ADC
-# from module.keyboard import Keyboard
-# from module.system_button import SystemButton
-# from module.cli_commands import CommandExecutor
-# from module.serial_handler import SerialHandler
-# from module.logger import configure_logging
-# from module.rotary_encoder import SimpleRotaryEncoder
-# from module.PWMcontroller import PWMController
-# from module.sensor_detect import SensorDetect
-# from module.mediator import Mediator
-# from module.dmesg_monitor import DmesgMonitor
-# from module.redis_listener import RedisListener
-
-# MODULES_OUTPUT_TO_SERIAL = ['cinepi_controller'] 
-
-# if __name__ == "__main__":
-
-#     logger, log_queue = configure_logging(MODULES_OUTPUT_TO_SERIAL)
-    
-#     # Detect sensor
-#     sensor_detect = SensorDetect()
-    
-#     # Instantiate the PWMController
-#     pwm_controller = PWMController(sensor_detect, PWM_pin=19)
-
-#     # Instantiate the CinePi instance
-#     cinepi_app = CinePi()
-
-#     # Instantiate other necessary components
-#     redis_controller = RedisController()
-#     ssd_monitor = SSDMonitor()
-#     usb_monitor = USBMonitor()
-#     gpio_output = GPIOOutput(rec_out_pin=[6, 21])
-
-#     # Instantiate the CinePiController with all necessary components and settings
-#     cinepi_controller = CinePiController(pwm_controller,
-#                                         redis_controller,
-#                                         usb_monitor, 
-#                                         ssd_monitor,
-#                                         sensor_detect,
-#                                         iso_steps=[100, 200, 400, 640, 800, 1200, 1600, 2500, 3200],                # Array for selectable ISO values
-#                                         shutter_a_steps=sorted([num for num in range(1, 361)] + [172.8, 346.6]),
-#                                         fps_steps=None
-#                                         )
-
-#     # Instantiate the AnalogControls component
-#     analog_controls = AnalogControls(cinepi_controller, iso_pot=0, shutter_a_pot=2, fps_pot=4)
-
-#     # Instantiate the GPIOControls component
-#     gpio_input = GPIOInput(cinepi_controller, redis_controller,
-#                             rec_button=[4,5],
-#                             iso_inc_button=27,    
-#                             iso_dec_button=10,
-#                             res_switch=None,
-#                             pwm_switch=22, 
-#                             shutter_a_sync_switch=16, 
-#                             fps_button=12,
-#                             fps_switch=None,
-#                             pot_lock_switch=24,
-#                             )     
-#     # Instantiate SystemButton
-#     system_button = SystemButton(cinepi_controller, redis_controller, ssd_monitor, system_button_pin=26)
-
-                                
-#     #Instantiate a rotary encoder for ISO control
-#     iso_encoder = SimpleRotaryEncoder(
-#                             cinepi_controller,
-#                             setting="iso",
-#                             pin_a=9,    #9
-#                             pin_b=11)   #11
-    
-#     #Instantiate a rotary encoder for shutter angle control
-#     shu_encoder = SimpleRotaryEncoder(
-#                             cinepi_controller,
-#                             setting="shutter_a_nom",
-#                             pin_a=23,   #23
-#                             pin_b=25)   #25
-    
-#     #Instantiate a rotary encoder for fps control
-#     fps_encoder = SimpleRotaryEncoder(
-#                             cinepi_controller,
-#                             setting="fps",
-#                             pin_a=8,    #8
-#                             pin_b=7)    #7
-
-#     # Instantiate the Mediator and pass the components to it
-#     mediator = Mediator(cinepi_app, redis_controller, usb_monitor, ssd_monitor, gpio_output)
-
-#     # Only after the mediator has been set up and subscribed to the events,
-#     # we can trigger methods that may cause the events to fire.
-#     usb_monitor.check_initial_devices()
-    
-#     keyboard = Keyboard(cinepi_controller, usb_monitor)
-    
-#     # Instantiate the CommandExecutor with all necessary components and settings
-#     command_executor = CommandExecutor(cinepi_controller, system_button)
-
-#     # Start the CommandExecutor thread
-#     command_executor.start()
-    
-#     serial_handler = SerialHandler(command_executor.handle_received_data, 9600, log_queue=log_queue)
-#     serial_handler.start()
-    
-#     dmesg_monitor = DmesgMonitor("/var/log/kern.log")
-#     dmesg_monitor.start() 
-    
-#     redis_listener = RedisListener(redis_controller)
-    
-#     simple_gui = SimpleGUI(pwm_controller, 
-#                            redis_controller, 
-#                            cinepi_controller, 
-#                            usb_monitor, 
-#                            ssd_monitor, 
-#                            serial_handler,
-#                            dmesg_monitor
-#                            )
-
-#     # Log initialization complete message
-#     logging.info(f"--- initialization complete")
-
-#     try:
-#         redis_controller.set_value('is_recording', 0)
-#         redis_controller.set_value('is_writing', 0)
-#         # Pause program execution, keeping it running until interrupted
-#         pause()
-#     except Exception:
-#         logging.error("An unexpected error occurred:\n" + traceback.format_exc())
-#         sys.exit(1)
-#     finally:
-#         # Reset trigger mode to deafult 0
-#         pwm_controller.stop_pwm()
-#         pwm_controller.set_trigger_mode(0)
-#         # Reset redis values to default                              )
-#         redis_controller.set_value('fps', 24)
-#         redis_controller.set_value('is_recording', 0)
-#         redis_controller.set_value('is_writing', 0)
-        
-#         # Set recording status to 0  
-#         gpio_output.set_recording(0)
-        
-#         dmesg_monitor.join()
-#         serial_handler.join()
-#         command_executor.join()
-        
-#         # Cleanup GPIO pins
-#         GPIO.cleanup()
--- src/module/simple_gui.py
+++ src/module/simple_gui.py
@@ -122,7 +122,14 @@ class SimpleGUI(threading.Thread):
             if self.cinepi_controller.gui_layout == 0:
 
                 # GUI Upper line
+                
+                # if self.cinepi_controller.iso_lock == False:
+                #     # Draw a grey box around the ISO text
+                #     iso_box_coords = (0, -5, 92, 34)  # Adjust these coordinates as needed
+                #     draw.rectangle(iso_box_coords, fill=(100,100,100,255))
+                
                 draw.text((10, -7), str(self.iso), font=font, fill="white")
+
                 if self.cinepi_controller.pwm_mode == False:
                     draw.text((110, -7), str(self.shutter_a), font=font, fill="white")
                     draw.text((205, -7), str(self.fps), font=font, fill="white")
@@ -224,56 +231,56 @@ class SimpleGUI(threading.Thread):
                 if '/dev/ttyACM0' in self.serial_handler.current_ports:
                     draw.text((10, 990), 'SER', font=font4, fill="white")
 
-            if self.cinepi_controller.gui_layout == 2:
+            # if self.cinepi_controller.gui_layout == 2:
                 
-                # GUI Upper line
-                draw.text((-3, -7), str(self.iso), font=font, fill=("white"))
-                if self.cinepi_controller.pwm_mode == False:
-                    draw.text((-3, 80), str(self.shutter_a), font=font, fill="white")
-                    draw.text((-3, 167), str(self.fps), font=font, fill="white")
-                elif self.cinepi_controller.pwm_mode == True:
-                    draw.text((-3, 80), str(self.pwm_controller.shutter_angle), font=font, fill="lightgreen")
-                    draw.text((-3, 167), str(int(round(float(self.pwm_controller.fps),0))), font=font, fill="lightgreen")
+            #     # GUI Upper line
+            #     draw.text((-3, -7), str(self.iso), font=font, fill=("white"))
+            #     if self.cinepi_controller.pwm_mode == False:
+            #         draw.text((-3, 80), str(self.shutter_a), font=font, fill="white")
+            #         draw.text((-3, 167), str(self.fps), font=font, fill="white")
+            #     elif self.cinepi_controller.pwm_mode == True:
+            #         draw.text((-3, 80), str(self.pwm_controller.shutter_angle), font=font, fill="lightgreen")
+            #         draw.text((-3, 167), str(int(round(float(self.pwm_controller.fps),0))), font=font, fill="lightgreen")
                     
-                if self.cinepi_controller.fps_double == True:
-                    draw.text((-3, 260), 'FPS SW', font=font, fill="lightgreen")
-                    draw.text((-3, 167), str(self.fps), font=font, fill="lightgreen")
+            #     if self.cinepi_controller.fps_double == True:
+            #         draw.text((-3, 260), 'FPS SW', font=font, fill="lightgreen")
+            #         draw.text((-3, 167), str(self.fps), font=font, fill="lightgreen")
                 
-                draw.text((-3, 340), str(self.cinepi_controller.exposure_time_fractions), font=font, fill="white")
+            #     draw.text((-3, 340), str(self.cinepi_controller.exposure_time_fractions), font=font, fill="white")
 
-                if self.cinepi_controller.pwm_mode == True:
-                    draw.text((-3, 427), 'PWM', font=font3, fill="lightgreen")
+            #     if self.cinepi_controller.pwm_mode == True:
+            #         draw.text((-3, 427), 'PWM', font=font3, fill="lightgreen")
 
-                if self.cinepi_controller.shutter_a_sync == True:
-                    draw.text((-3, 495), 'SYNC', font=font3, fill="white")
-                    draw.text((-3, 540), str(self.shutter_a_nom), font=font, fill="white")
+            #     if self.cinepi_controller.shutter_a_sync == True:
+            #         draw.text((-3, 495), 'SYNC', font=font3, fill="white")
+            #         draw.text((-3, 540), str(self.shutter_a_nom), font=font, fill="white")
                     
-                if self.cinepi_controller.parameters_lock == True:
-                    draw.text((-3, 610), 'LOCK', font=font3, fill=(255,0,0,255))
+            #     if self.cinepi_controller.parameters_lock == True:
+            #         draw.text((-3, 610), 'LOCK', font=font3, fill=(255,0,0,255))
                 
-                if self.dmesg_monitor.undervoltage_flag:
-                    if self.is_recording:
-                        draw.text((-3, 680), str('VOLTAGE'), font=font, fill="black")
-                    else:
-                        draw.text((-3, 680), str('VOLTAGE'), font=font, fill="yellow")   
+            #     if self.dmesg_monitor.undervoltage_flag:
+            #         if self.is_recording:
+            #             draw.text((-3, 680), str('VOLTAGE'), font=font, fill="black")
+            #         else:
+            #             draw.text((-3, 680), str('VOLTAGE'), font=font, fill="yellow")   
 
-                draw.text((1862, -7), str(self.cpu_load), font=font4, fill="white")
-                draw.text((1862, 21), str(self.cpu_temp), font=font4, fill="white")
+            #     draw.text((1862, -7), str(self.cpu_load), font=font4, fill="white")
+            #     draw.text((1862, 21), str(self.cpu_temp), font=font4, fill="white")
                 
-                # GUI Lower line
-                if self.min_left:
-                    draw.text((-3, 1044), str(self.min_left) + " MIN", font=font4, fill="white")
-                else:
-                    draw.text((-3, 1044), 'NO DISK', font=font4, fill="white")
+            #     # GUI Lower line
+            #     if self.min_left:
+            #         draw.text((-3, 1044), str(self.min_left) + " MIN", font=font4, fill="white")
+            #     else:
+            #         draw.text((-3, 1044), 'NO DISK', font=font4, fill="white")
                     
-                if self.usb_monitor.usb_mic:
-                    draw.text((-3, 910), 'MIC', font=font4, fill="white")
+            #     if self.usb_monitor.usb_mic:
+            #         draw.text((-3, 910), 'MIC', font=font4, fill="white")
                     
-                if self.usb_monitor.usb_keyboard:
-                    draw.text((-3, 950), 'KEY', font=font4, fill="white")
+            #     if self.usb_monitor.usb_keyboard:
+            #         draw.text((-3, 950), 'KEY', font=font4, fill="white")
 
-                if '/dev/ttyACM0' in self.serial_handler.current_ports:
-                    draw.text((-3, 990), 'SER', font=font4, fill="white")
+            #     if '/dev/ttyACM0' in self.serial_handler.current_ports:
+            #         draw.text((-3, 990), 'SER', font=font4, fill="white")
 
             self.fb.show(image)
         except OSError as e:
@@ -306,3 +313,160 @@ class SimpleGUI(threading.Thread):
             error_msg += "Traceback: "
             error_msg += ''.join(traceback.format_tb(e.__traceback__))
             print(error_msg)
+
+# Here is the complete script with detailed implementation, including dummy placeholders
+# for external module references and methods that need to be filled with actual logic.
+
+# import os
+# import time
+# import threading
+# from PIL import Image, ImageDraw, ImageFont
+# import subprocess
+# import logging
+
+# import logging
+# import traceback
+
+# # Setting up basic logging configuration
+# logging.basicConfig(level=logging.INFO, filename='simple_gui.log', filemode='a',
+#                     format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+# # Placeholder for the Framebuffer class and other external modules not defined here
+# class Framebuffer:
+#     def __init__(self, index):
+#         self.size = (1920, 1080)  # Default resolution
+#     def show(self, image):
+#         pass  # Placeholder for displaying an image on the framebuffer
+
+# class SimpleGUI(threading.Thread):
+#     def __init__(self, pwm_controller, redis_controller, cinepi_controller, usb_monitor, ssd_monitor, serial_handler, dmesg_monitor):
+#         threading.Thread.__init__(self)
+
+#         # Controllers and monitors
+#         self.pwm_controller = pwm_controller
+#         self.redis_controller = redis_controller
+#         self.cinepi_controller = cinepi_controller
+#         self.usb_monitor = usb_monitor
+#         self.ssd_monitor = ssd_monitor
+#         self.serial_handler = serial_handler
+#         self.dmesg_monitor = dmesg_monitor
+
+#         # Paths
+#         self.current_directory = os.path.dirname(os.path.abspath(__file__))
+#         self.relative_path_to_font = os.path.join(self.current_directory, '../../resources/fonts/SFCompactRounded.ttf')
+
+#         # Framebuffer
+#         self.fb = Framebuffer(0)
+#         self.disp_width, self.disp_height = self.fb.size
+
+#         # Initial values
+#         self.init_values()
+
+#         # Hide the cursor and start GUI
+#         self.hide_cursor()
+#         self.check_fb_existence()
+#         self.start()
+        
+#         logging.info(f"Simple GUI instantiated. HDMI {self.fb.size}")
+
+#     def init_values(self):
+#         """Initialize or reset GUI values."""
+#         self.iso = "100"
+#         self.shutter_a = "1/50"
+#         self.fps = 24
+#         self.is_recording = False
+#         self.min_left = 120
+#         self.cpu_load = "5%"
+#         self.cpu_temp = "60°C"
+
+#     def get_values(self):
+#         """Fetch and update GUI values."""
+#         # Example fetching method, needs implementation
+#         # This is where you would update the properties like self.iso, self.shutter_a, etc.
+#         pass
+
+#     def hide_cursor(self):
+#         """Hide the cursor in the framebuffer display."""
+#         try:
+#             subprocess.call(["sudo", "sh", "-c", "echo 0 > /sys/class/graphics/fbcon/cursor_blink"])
+#         except Exception as e:
+#             print(f"Error occurred while hiding cursor: {e}")
+
+#     def check_fb_existence(self):
+#         """Check framebuffer device existence and log if not found."""
+#         fb_path = "/dev/fb0"
+#         if not os.path.exists(fb_path):
+#             logging.info("No HDMI display found")
+
+#     def draw_objects(self, layout_id):
+#         try:
+#             """Draw GUI elements based on the layout and actual values."""
+#             layout = self.define_layouts()[layout_id]
+#             actual_values = self.populate_values()
+
+#             # if self.is_recording == 1:
+#             #     self.fill_color = "red"
+#             # else:
+#             self.fill_color = "black"
+#             image = Image.new("RGBA", self.fb.size)
+#             draw = ImageDraw.Draw(image)
+#             draw.rectangle(((0, 0), self.fb.size), fill=self.fill_color)
+
+#             for key, value in layout.items():
+#                 coords = value['coords']
+#                 font_size = value['font_size']
+#                 color = value.get('color', 'white')  # Default to white if color is not specified
+#                 text = str(actual_values.get(key, ''))  # Default to empty string if key not found
+
+#                 font = ImageFont.truetype(os.path.realpath(self.relative_path_to_font), font_size)
+#                 draw.text(coords, text, font=font, fill=color)
+
+#             self.fb.show(image)
+#         except OSError as e:
+#             print(f"Error occurred in draw_display: {e}")
+
+#     def define_layouts(self):
+#         """Define GUI layouts with their properties."""
+#         return {
+#             0: {
+#                 'iso': {'coords': (10, -7), 'font_size': 24, 'color': 'white'},
+#                 'shutter_speed': {'coords': (10, 40), 'font_size': 24, 'color': 'white'},
+#                 'fps': {'coords': (10, 70), 'font_size': 24, 'color': 'white'},
+#                 'cpu_load': {'coords': (10, 100), 'font_size': 24, 'color': 'white'},
+#                 'cpu_temp': {'coords': (10, 130), 'font_size': 24, 'color': 'white'},
+#             },
+#             1: {
+#                 # Different layout for demonstration
+#             },
+#             2: {
+#                 # Different layout for demonstration
+#             },
+#         }
+
+#     def populate_values(self):
+#         """Populate a dictionary with actual values for GUI elements."""
+#         #self.get_values()  # Ensure values are updated
+#         return {
+#             'iso': self.iso,
+#             'shutter_speed': self.shutter_a,
+#             'fps': self.fps,
+#             'cpu_load': self.cpu_load,
+#             'cpu_temp': self.cpu_temp,
+#         }
+
+#     def run(self):
+#         """Main GUI loop."""
+
+#         try:
+#             while True:
+#                 self.draw_objects(self.cinepi_controller.gui_layout)
+#                 time.sleep(0.2)
+
+#         except Exception as e:
+#             error_msg = f"Error occurred in 'run' loop of SimpleGUI: {e}\n"
+#             error_msg += "Exception type: " + str(type(e)) + "\n"
+#             error_msg += "Exception args: " + str(e.args) + "\n"
+#             error_msg += "Traceback: "
+#             error_msg += ''.join(traceback.format_tb(e.__traceback__))
+#             print(error_msg)

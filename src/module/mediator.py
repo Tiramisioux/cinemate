7b203a9d3ead7f8b50b88925024b7b2243ab44ea
Patrik Eriksson <patrik.m.eriksson@gmail.com>
2024-04-08 00:54:51 +0200

    fixed usb and ssd monitor - all ssd drives should now be compatible

--- src/module/mediator.py
+++ src/module/mediator.py
@@ -12,8 +12,11 @@ class Mediator:
         self.ssd_monitor = ssd_monitor
         self.gpio_output = gpio_output
         
+        # # Load recognized SSD models from the settings file
+        # self.recognized_ssds = self.load_ssd_settings("/home/pi/cinemate/src/module/ssd_settings.json")
+        
         self.cinepi_app.message.subscribe(self.handle_cinepi_message)
-
+        # self.usb_monitor.usb_event.subscribe(self.handle_usb_event)
         self.ssd_monitor.write_status_changed_event.subscribe(self.handle_write_status_change)
 
         # Subscribe to the "is_recording" key changes
@@ -24,6 +27,11 @@ class Mediator:
         
          # Subscribe to the "fps_actual" key changes
         self.redis_controller.redis_parameter_changed.subscribe(self.handle_fps_actual_change)
+        
+         # Subscribe to SSD unmount events
+        self.ssd_monitor.unmount_event.subscribe(self.handle_ssd_unmount)
+        
+        self.ssd_monitor.ssd_event.subscribe(self.handle_ssd_event)
         
     def load_ssd_settings(self, settings_file):
         """Load SSD settings from the specified JSON file."""
@@ -38,7 +46,25 @@ class Mediator:
     def handle_cinepi_message(self, message):
         # Handle CinePi app messages (currently not logging)
         pass  
+    
+    # def handle_usb_event(self, action, device, device_model, device_serial):
+    #     """Handle USB events with SSD recognition based on the settings file."""
+    #     model_upper = device_model.upper()
+    #     if any(ssd_model.upper() in model_upper for ssd_model in self.recognized_ssds):
+    #         # The device is recognized as an SSD from the settings file
+    #         logging.info(f"Recognized SSD connected: {device_model}")
+    #         logging.info(f"SSD device serial: {device_serial}")
+    #         self.ssd_monitor.update(action, device_model, device_serial)
+    #         self.ssd_monitor.usb_hd_serial = device_serial  # Set the serial number
 
+    #     # Add else condition if needed to handle non-SSD USB events
+
+    def handle_ssd_event(self, action, message):
+        # Handle SSD events
+        logging.info(f"SSDMonitor says: {action} {message}")
+        self.ssd_monitor.get_ssd_space_left()
+        space_left = self.ssd_monitor.last_space_left
+        logging.info('Space left:', space_left)
         
     def handle_ssd_unmount(self):
         logging.info("SSD unmounted.")
@@ -60,7 +86,8 @@ class Mediator:
         # Reset the timer when recording starts (status = 1)
         elif status == 1:
             if self.stop_recording_timer and self.stop_recording_timer.is_alive():
-                self.stop_recording_timer.cancel()            
+                self.stop_recording_timer.cancel()
+                
 
     def handle_redis_event(self, data):
         # Handle "is_recording" key changes

5dd61fc8d654bce1f12015da8fb9e6f75b2c0e93
Patrik <patrik.m.eriksson@gmail.com>
2024-07-03 22:16:22 +0100

    fix

--- src/module/cinepi_app.py
+++ src/module/cinepi_app.py
@@ -28,26 +28,50 @@ class CinePi:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self, *cinepi_args):
+    def __init__(self, redis_controller, sensor_detect):
         if not hasattr(self, 'initialized'):  # only initialize once
             self.message = Event()
             self.suppress_output = False
-            self.cinepi_args = cinepi_args
-            
-            self.start_cinepi_process(cinepi_args)
+            self.redis_controller = redis_controller
+            self.sensor_detect = sensor_detect  # Initialize SensorDetect
+            self.default_args = self.get_default_args(sensor_detect)
+            self.process = None  # Initialize process as None
             self.initialized = True  # indicate that the instance has been initialized
             logging.info('CinePi instantiated')
 
-    def start_cinepi_process(self, cinepi_args):
-        command = ['cinepi-raw'] + list(cinepi_args)
-        self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def get_default_args(self, sensor_detect):
+        sensor_mode = int(self.redis_controller.get_value('sensor_mode'))
+        sensor_model = sensor_detect.camera_model
+        tuning_file_path = f'/home/pi/libcamera/src/ipa/rpi/pisp/data/{sensor_model}.json'
         
+        return [
+            '--mode', f"{sensor_detect.get_width(sensor_model, sensor_mode)}:{sensor_detect.get_height(sensor_model, sensor_mode)}:{sensor_mode}:U",
+            '--width', str(sensor_detect.get_width(sensor_model, sensor_mode)),
+            '--height', str(sensor_detect.get_height(sensor_model, sensor_mode)),
+            '--lores-width', str(sensor_detect.get_lores_width(sensor_model, sensor_mode)),
+            '--lores-height', str(sensor_detect.get_lores_height(sensor_model, sensor_mode)),
+            '--hdr', 'sensor',
+            '-p', '0,30,1920,1020',
+            '--post-process-file', '/home/pi/post-processing.json',
+            '--tuning-file', tuning_file_path
+        ]
+
+    def start_cinepi_process(self, cinepi_args=None):
+        # Ensure default arguments are included
+        if cinepi_args is None:
+            cinepi_args = self.default_args
+        else:
+            cinepi_args = list(cinepi_args) + self.default_args
+
+        command = ['cinepi-raw'] + cinepi_args
+        self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
         self.out_queue = Queue()
         self.err_queue = Queue()
 
         self.out_thread = Thread(target=enqueue_output, args=(self.process.stdout, self.out_queue, self.message))
         self.err_thread = Thread(target=enqueue_output, args=(self.process.stderr, self.err_queue, self.message))
-        
+
         self.out_thread.daemon = True
         self.err_thread.daemon = True
         self.out_thread.start()
@@ -55,19 +79,16 @@ class CinePi:
 
     def shutdown(self):
         """Shut down the CinePi instance."""
-        logging.info('Shutting down CinePi instance.')
-        self.process.terminate()
-        self.process.wait()
-        logging.info('CinePi instance shut down.')
+        if self.process is not None:
+            logging.info('Shutting down CinePi instance.')
+            self.process.terminate()
+            self.process.wait()
+            logging.info('CinePi instance shut down.')
 
-    def restart(self, *new_args):
+    def restart(self):
         """Restart the CinePi instance."""
         logging.info('Restarting CinePi instance.')
         self.shutdown()
-        
-        # Update the args for the new process
-        self.cinepi_args = new_args
-        
-        # Restart the process with new arguments
-        self.start_cinepi_process(new_args)
+        # Restart the process with default arguments
+        self.start_cinepi_process()
         logging.info('CinePi instance restarted.')

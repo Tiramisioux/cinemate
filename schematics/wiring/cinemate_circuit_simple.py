6199c314657740baf21c5e36e5d24645ea8e773c
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2025-02-04 19:33:02 +0100

    Update cinemate_circuit_simple.py

--- schematics/wiring/cinemate_circuit_simple.py
+++ schematics/wiring/cinemate_circuit_simple.py
@@ -1,32 +1,32 @@
 from skidl import *
 
-# Define a Custom 40-Pin Header Instead of Relying on KiCad Libraries
-rpi = Part('Device', 'J', value="RaspberryPi_40Pin", footprint='Connector_Generic:Conn_02x20')
+# Define a custom 40-pin header manually
+rpi = SubCircuit(name="RaspberryPi_40Pin")
 
-# Manually Define 40 GPIO Pins for Raspberry Pi
+# Manually define 40 GPIO Pins (Generic Header)
 rpi.pins = [Pin(num=i, name=f"GPIO{i}", func=Pin.BIDIR) for i in range(1, 41)]
 
 # Define Components
-button_rec = Part('Device', 'SW_Push', footprint='Button')  # Start/Stop Recording
-button_iso_inc = Part('Device', 'SW_Push', footprint='Button')  # Increase ISO
-button_iso_dec = Part('Device', 'SW_Push', footprint='Button')  # Decrease ISO
-led_rec = Part('Device', 'LED', footprint='LED')
-resistor = Part('Device', 'R', value='220', footprint='Resistor')
+button_rec = Part('Device', 'SW_Push')  # Start/Stop Recording
+button_iso_inc = Part('Device', 'SW_Push')  # Increase ISO
+button_iso_dec = Part('Device', 'SW_Push')  # Decrease ISO
+led_rec = Part('Device', 'LED')
+resistor = Part('Device', 'R', value='220')
 
 # Connect Buttons to GPIO Pins
-rpi[4] += button_rec[1]
-button_rec[2] += rpi[39]  # Connect to Ground
+rpi.pins[4] += button_rec[1]
+button_rec[2] += rpi.pins[39]  # Connect to Ground
 
-rpi[17] += button_iso_inc[1]
-button_iso_inc[2] += rpi[39]  # Connect to Ground
+rpi.pins[17] += button_iso_inc[1]
+button_iso_inc[2] += rpi.pins[39]  # Connect to Ground
 
-rpi[14] += button_iso_dec[1]
-button_iso_dec[2] += rpi[39]  # Connect to Ground
+rpi.pins[14] += button_iso_dec[1]
+button_iso_dec[2] += rpi.pins[39]  # Connect to Ground
 
 # Connect Rec Light LED
-rpi[6] += led_rec[1]
+rpi.pins[6] += led_rec[1]
 led_rec[2] += resistor[1]
-resistor[2] += rpi[39]  # Connect to Ground
+resistor[2] += rpi.pins[39]  # Connect to Ground
 
 # Generate netlist
 generate_netlist()

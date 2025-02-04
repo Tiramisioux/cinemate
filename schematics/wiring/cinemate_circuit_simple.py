572119f1547ba80c04337fe5f7f279d1f6c5d7b1
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2025-02-04 19:28:08 +0100

    Update cinemate_circuit_simple.py

--- schematics/wiring/cinemate_circuit_simple.py
+++ schematics/wiring/cinemate_circuit_simple.py
@@ -6,21 +6,27 @@ rpi = Part('Device', 'J', value="RaspberryPi_40Pin", footprint='Connector_Generi
 # Manually Define 40 GPIO Pins for Raspberry Pi
 rpi.pins = [Pin(num=i, name=f"GPIO{i}", func=Pin.BIDIR) for i in range(1, 41)]
 
-# Define Buttons
-button_rec = Part('Device', 'SW_Push', footprint='Button')
-
-# Define LED and Resistor
+# Define Components
+button_rec = Part('Device', 'SW_Push', footprint='Button')  # Start/Stop Recording
+button_iso_inc = Part('Device', 'SW_Push', footprint='Button')  # Increase ISO
+button_iso_dec = Part('Device', 'SW_Push', footprint='Button')  # Decrease ISO
 led_rec = Part('Device', 'LED', footprint='LED')
 resistor = Part('Device', 'R', value='220', footprint='Resistor')
 
-# Connect Components
+# Connect Buttons to GPIO Pins
 rpi[4] += button_rec[1]
 button_rec[2] += rpi[39]  # Connect to Ground
 
+rpi[17] += button_iso_inc[1]
+button_iso_inc[2] += rpi[39]  # Connect to Ground
+
+rpi[14] += button_iso_dec[1]
+button_iso_dec[2] += rpi[39]  # Connect to Ground
+
+# Connect Rec Light LED
 rpi[6] += led_rec[1]
 led_rec[2] += resistor[1]
 resistor[2] += rpi[39]  # Connect to Ground
 
-# Generate Netlist
+# Generate netlist
 generate_netlist()
-

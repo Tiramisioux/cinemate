03b8a35043c03dbb67c9ef5ba9b5b0d6a77fc62d
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2025-02-04 19:40:10 +0100

    Update cinemate_circuit_advanced.py

--- schematics/wiring/cinemate_circuit_advanced.py
+++ schematics/wiring/cinemate_circuit_advanced.py
@@ -1,10 +1,13 @@
 from skidl import *
 
-# Define a custom 40-pin header manually
-rpi = SubCircuit(name="RaspberryPi_40Pin")
+# Create a Raspberry Pi 40-Pin Header using SKiDL, NO KiCad library needed!
+class RaspberryPi40Pin(SubCircuit):
+    def __init__(self):
+        super().__init__("RaspberryPi_40Pin")
+        self.pins = [Pin(num=i, name=f"GPIO{i}", func=Pin.BIDIR) for i in range(1, 41)]
 
-# Manually define 40 GPIO Pins (Generic Header)
-rpi.pins = [Pin(num=i, name=f"GPIO{i}", func=Pin.BIDIR) for i in range(1, 41)]
+# Create RPi as a SubCircuit
+rpi = RaspberryPi40Pin()
 
 # Define Buttons
 buttons = {
@@ -39,43 +42,43 @@ led_rec = Part('Device', 'LED')
 resistor = Part('Device', 'R', value='220')
 
 # Connect Buttons to GPIOs
-rpi.pins[4] += buttons["rec"][1]
+rpi.pins[3] += buttons["rec"][1]
 buttons["rec"][2] += rpi.pins[39]
 
-rpi.pins[17] += buttons["iso_inc"][1]
+rpi.pins[5] += buttons["iso_inc"][1]
 buttons["iso_inc"][2] += rpi.pins[39]
 
-rpi.pins[14] += buttons["iso_dec"][1]
+rpi.pins[7] += buttons["iso_dec"][1]
 buttons["iso_dec"][2] += rpi.pins[39]
 
-rpi.pins[12] += buttons["fps_toggle"][1]
+rpi.pins[11] += buttons["fps_toggle"][1]
 buttons["fps_toggle"][2] += rpi.pins[39]
 
-rpi.pins[26] += buttons["system"][1]
+rpi.pins[13] += buttons["system"][1]
 buttons["system"][2] += rpi.pins[39]
 
 # Connect Rotary Encoders
-rpi.pins[9] += encoders["iso"][1]
-rpi.pins[11] += encoders["iso"][2]
+rpi.pins[15] += encoders["iso"][1]
+rpi.pins[16] += encoders["iso"][2]
 
-rpi.pins[23] += encoders["shutter"][1]
-rpi.pins[25] += encoders["shutter"][2]
+rpi.pins[18] += encoders["shutter"][1]
+rpi.pins[19] += encoders["shutter"][2]
 
-rpi.pins[7] += encoders["fps"][1]
-rpi.pins[8] += encoders["fps"][2]
+rpi.pins[21] += encoders["fps"][1]
+rpi.pins[22] += encoders["fps"][2]
 
 # Connect Potentiometers for Analog Control
-rpi.pins[1] += pot_iso[1]
-rpi.pins[2] += pot_shutter[1]
-rpi.pins[3] += pot_fps[1]
+rpi.pins[23] += pot_iso[1]
+rpi.pins[24] += pot_shutter[1]
+rpi.pins[26] += pot_fps[1]
 
 # Connect Switches
-rpi.pins[24] += switches["shutter_lock"][1]
-rpi.pins[16] += switches["shutter_sync"][1]
-rpi.pins[22] += switches["pwm_toggle"][1]
+rpi.pins[29] += switches["shutter_lock"][1]
+rpi.pins[31] += switches["shutter_sync"][1]
+rpi.pins[33] += switches["pwm_toggle"][1]
 
 # Connect Rec Light LED
-rpi.pins[6] += led_rec[1]
+rpi.pins[8] += led_rec[1]
 led_rec[2] += resistor[1]
 resistor[2] += rpi.pins[39]
 

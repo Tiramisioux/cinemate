e66d04deaa5441f35203aa4182e9943d46951755
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2025-02-04 19:27:31 +0100

    Update cinemate_circuit_simple.py

--- schematics/wiring/cinemate_circuit_simple.py
+++ schematics/wiring/cinemate_circuit_simple.py
@@ -1,36 +1,26 @@
 from skidl import *
 
-# Define KiCad Symbol Library Paths Manually (if missing)
-import os
-os.environ["KICAD_SYMBOL_DIR"] = "/usr/share/kicad/symbols"
+# Define a Custom 40-Pin Header Instead of Relying on KiCad Libraries
+rpi = Part('Device', 'J', value="RaspberryPi_40Pin", footprint='Connector_Generic:Conn_02x20')
 
-# Define Raspberry Pi as a generic 40-pin connector
-rpi = Part('Connector_Generic', 'Conn_02x20_Odd_Even', footprint='Connector_Generic:Conn_02x20_Odd_Even')
-
+# Manually Define 40 GPIO Pins for Raspberry Pi
+rpi.pins = [Pin(num=i, name=f"GPIO{i}", func=Pin.BIDIR) for i in range(1, 41)]
 
 # Define Buttons
-button_rec = Part('Device', 'SW_Push', footprint='Button')  # Start/Stop Recording
-button_iso_inc = Part('Device', 'SW_Push', footprint='Button')  # Increase ISO
-button_iso_dec = Part('Device', 'SW_Push', footprint='Button')  # Decrease ISO
+button_rec = Part('Device', 'SW_Push', footprint='Button')
 
-# Define LED and Resistor for Recording Indicator
+# Define LED and Resistor
 led_rec = Part('Device', 'LED', footprint='LED')
 resistor = Part('Device', 'R', value='220', footprint='Resistor')
 
-# Connect Buttons to GPIO Pins
+# Connect Components
 rpi[4] += button_rec[1]
 button_rec[2] += rpi[39]  # Connect to Ground
 
-rpi[17] += button_iso_inc[1]
-button_iso_inc[2] += rpi[39]  # Connect to Ground
-
-rpi[14] += button_iso_dec[1]
-button_iso_dec[2] += rpi[39]  # Connect to Ground
-
-# Connect Rec Light LED
 rpi[6] += led_rec[1]
 led_rec[2] += resistor[1]
 resistor[2] += rpi[39]  # Connect to Ground
 
-# Generate netlist
+# Generate Netlist
 generate_netlist()
+

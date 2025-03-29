f3dc723928d29d4e019d711dad7b6a1965152925
Patrik Eriksson <patrik.m.eriksson@gmail.com>
2025-03-29 21:00:32 +0000

    update simple_gui.py with more dynamic system boxes + serial_handler.py logic

--- src/module/serial_handler.py
+++ src/module/serial_handler.py
@@ -12,30 +12,29 @@ class SerialHandler(threading.Thread):
         self.baudrate = baudrate
         self.timeout = timeout
         self.callback = callback
-        
+
         self.log_queue = log_queue
-        
+
         self.portlist = ['/dev/ttyACM0', '/dev/serial0', '/dev/ttyS0']
-         
         self.current_ports = []
 
-        # Start with checking available poarts during the creation of the object 
+        self.serial_connected = False  # Flag to indicate USB serial device connection
+
+        self.running = True
+        self.last_received_time = {}  # Track last received time for each port
+
+        # Initial scan for available ports
         self.update_available_ports()
 
         if len(self.serials) == 0:
             logging.info("No USB serial devices")
 
-        self.running = True
-        
-        self.last_received_time = {}  # Initialize a dictionary to store timestamps for each port
-
     def write_to_ports(self, message):
         for ser in self.serials:
             try:
-                # Ensure the message ends with a newline character
                 if not message.endswith('\n'):
                     message += '\n'
-                    
+
                 if 'Sending' not in message:
                     ser.write(message.encode('utf-8'))
                     logging.info(f"Sending on {ser.port} {message}")
@@ -47,96 +46,75 @@ class SerialHandler(threading.Thread):
                 ser.close()
 
     def read_from_ports(self):
-
         responses = []
         threshold_time = 0  # 100 ms threshold
 
-        for ser in self.serials:
+        for ser in self.serials[:]:
             try:
                 while ser.in_waiting:
                     line = ser.readline()
-
-                    # Decoding binary line to UTF-8 string, ignoring non-printable characters
                     string_line = line.decode('utf-8')
-                    
-                    null_string = r"b'\x00'"
-                    
-                    # Check if the line is a NULL character
-                    if null_string in string_line:
+
+                    if r"b'\x00'" in string_line:
                         logging.info(f"Received NULL from {ser.port}. Ignoring.")
                         continue
-                    
-                    # Check if message on the same port is received within the threshold time
+
                     current_time = time.time()
                     last_time = self.last_received_time.get(ser.port, 0)
                     if current_time - last_time < threshold_time:
                         logging.info(f"Ignoring message received within {threshold_time * 1000} ms on {ser.port}")
                         continue
-                    
-                    # Update the last received time for the port
+
                     self.last_received_time[ser.port] = current_time
-                    
                     responses.append((ser.port, string_line.strip()))
 
             except (OSError, serial.SerialException) as e:
                 logging.error(f"Failed to read from {ser.port}: {e}")
                 logging.info(f"Failed to read from {ser.port}: {e}")
+                if ser.port == '/dev/ttyACM0':
+                    self.serial_connected = False
                 self.serials.remove(ser)
                 ser.close()
 
         if responses:
-            for port, response in responses:   
+            for port, response in responses:
                 logging.info(f'Received from {port}: {response}')
-                # Check if the response is not a NULL character before calling the callback
                 if not "b'\x00'" in response:
                     self.callback(response)
 
         return responses
 
     def update_available_ports(self):
-        # Close ports that are no longer available
-        for ser in self.serials:
+        for ser in self.serials[:]:
             if ser.port not in self.portlist:
                 ser.close()
                 self.serials.remove(ser)
 
-        # Open new available ports
+        self.current_ports = [ser.port for ser in self.serials]
+
         for port in self.portlist:
-
-            # Update current_ports at each iteration to reflect serials list state.
-            self.current_ports = [ser.port for ser in self.serials]
-
             if port not in self.current_ports:
                 try:
-                    self.serials.append(serial.Serial(port, self.baudrate, timeout=self.timeout))
+                    ser = serial.Serial(port, self.baudrate, timeout=self.timeout)
+                    self.serials.append(ser)
                     logging.info(f"Successfully opened port {port}")
+                    self.current_ports = [s.port for s in self.serials]
 
-                    # After opening a new port, update current_ports again
-                    self.current_ports = [ser.port for ser in self.serials]
-                    logging.info(f"current ports {self.current_ports}")
-                except serial.SerialException as e:
+                    if port == '/dev/ttyACM0':
+                        self.serial_connected = True
+
+                except serial.SerialException:
                     continue
- 
+
+        # Reconfirm state in case ACM0 was removed in previous step
+        if '/dev/ttyACM0' not in self.current_ports:
+            self.serial_connected = False
+
     def run(self):
         while self.running:
-            # Update available ports at each iteration
             self.update_available_ports()
-
             responses = self.read_from_ports()
             if responses:
-                for port, response in responses:   
-                    logging.info(f'Received from {port}: {response.strip()}') 
-                
-            # # Check the log queue and send log messages over the serial port
-            # try:
-            #     # if '/dev/serial0' in self.current_ports:
-            #     while not self.log_queue.empty():  # Process all log messages in the queue
-            #         log_message = self.log_queue.get_nowait()  # Non-blocking get
-            #         try:
-            #             self.write_to_ports(log_message)  # Write each log message to the ports
-            #         except Exception as e:  # Handle exceptions that occur while writing to the ports
-            #             logging.error(f"Failed to write log message to ports: {str(e)}")
-            # except queue.Empty:
-            #     print("Queue is Empty!")  # Debugging print
-
+                for port, response in responses:
+                    logging.info(f'Received from {port}: {response.strip()}')
             time.sleep(0.01)

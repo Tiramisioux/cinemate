baa66187a23876af58774d50cd2b16a61ce0dade
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2026-09-19 23:24:52 +0200

    Add regression coverage for ClearHDR binning metadata

--- _test/test_resolution_defaults.py
+++ _test/test_resolution_defaults.py
@@ -24,6 +24,38 @@ class ResolutionDefaultsTests(unittest.TestCase):
 
         self.assertIn(4, settings["image_capture"]["k_steps"])
 
+    def test_clearhdr_16bit_driver_binning_is_preserved(self):
+        detector = SensorDetect.__new__(SensorDetect)
+        detector.settings = {}
+        detector.k_steps = []
+        detector.bit_depths = []
+        detector.custom_modes = {}
+        detector.hdr_modes = [False, True]
+        detector.clear_hdr_depths = {16}
+        detector.sensor_database_file = "resources/sensors.json"
+        detector.sensor_database = detector._load_sensor_database()
+        detector.packing_info = detector._packing_info_from_database()
+
+        output = """
+0 : imx585 [3840x2160 16-bit RGGB] (/base/i2c/imx585@1a)
+    Modes: 'SRGGB16' : 1440x1100 [30.00 fps - (240, 0)/1440x1080 crop; binning 2x2; mode-crop (480,0)/2880x2160]
+                               1920x1100 [30.00 fps - (0, 0)/1920x1080 crop; binning 2x2; mode-crop (0,0)/3840x2160]
+                               1920x1120 [57.69 fps - (960, 540)/1920x1080 crop; binning 1x1; mode-crop (960,540)/1920x1080]
+                               2880x2200 [29.58 fps - (480, 0)/2880x2160 crop; binning 1x1; mode-crop (480,0)/2880x2160]
+                               3840x2200 [21.99 fps - (0, 0)/3840x2160 crop; binning 1x1; mode-crop (0,0)/3840x2160]
+"""
+        parsed = detector._parse_cinepi_output(output, hdr=True)["imx585"]
+
+        by_size = {(m["width"], m["height"]): m for m in parsed}
+        self.assertEqual((2, 2), (by_size[(1440, 1100)]["binning_x"],
+                                  by_size[(1440, 1100)]["binning_y"]))
+        self.assertEqual((2, 2), (by_size[(1920, 1100)]["binning_x"],
+                                  by_size[(1920, 1100)]["binning_y"]))
+        self.assertEqual((1, 1), (by_size[(1920, 1120)]["binning_x"],
+                                  by_size[(1920, 1120)]["binning_y"]))
+        self.assertEqual((1, 1), (by_size[(3840, 2200)]["binning_x"],
+                                  by_size[(3840, 2200)]["binning_y"]))
+
     def test_stock_filter_keeps_imx585_4k_mode(self):
         settings = json.loads(strip_jsonc(
             (ROOT / "resources/settings/settings_default.jsonc").read_text(encoding="utf-8")
